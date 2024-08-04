dbde727bb9e6282bcdd83ffe9d3b6b8e15fe5d42
okofka <nazar.dmytroo@gmail.com>
2024-08-04 15:12:50 +0300

    Add Player logic

--- KillingTimeGame/Assets/Scrpts/PlayerScript.cs
+++ KillingTimeGame/Assets/Scrpts/PlayerScript.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+
+    private int desiredLane = 1;
+    public float laneDistance = 1.5f;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction.z = forwardSpeed;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        Vector3 targerPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (desiredLane == 0)
+        {
+            targerPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane == 2)
+        { 
+            targerPosition += Vector3.right * laneDistance;
+        }
+
+        //transform.position = targerPosition;
+        transform.position = Vector3.Lerp(transform.position, targerPosition, 60 * Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
     }
 }
